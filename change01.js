9456d17f2a7c1baab23b45763436ebaee0143520
KingJi8 <jiwon.c.king@gmail.com>
2023-03-07 00:47:25 +0900

    build(rebuild): 불러오기와 함수 작동 분리 등
    - 대화 내용 최신 것부터 적용되도록 변경
    - 그룹 채팅이여도 가능 (요소를 추가하면 오른쪽에 위치하는 사람 선택 가능하도록 분리)
    
    - 기능별 함수 분리
    - 정해진 숫자만큼 로딩되도록 함수 생성
    
    1) 스크롤 액션이 window에서도, txtlog에서도 먹히지 않습니다.
    2) window가 아닌 요소 내부에서 스크롤을 하면 어떻게 조건을 세워야할지 모르겠습니다.

--- change01.js
+++ change01.js
@@ -1,112 +1,136 @@
 const $getFileBtn = document.getElementById("getFileBtn");
 const $secpg = document.querySelector("#secPage .container");
+const $txtLog = document.getElementById("txtlog");
+const talker = [];
+const bubbleSplit = [];
+const fileChanges = [];
+const loadingAmount = 20;
 
+// 날짜 구성
+const $wholeDate = document.createElement('div');
+
+function dateBubble(date){
+  let kkotxtdate = date.replaceAll("---------------","");
+  $wholeDate.classList.add('kkodatebox');
+  $wholeDate.innerHTML = kkotxtdate;
+}
+
+// 버블 구성
+function talkBubble(talker, talkTime, talkContent, talk){
+  if(talker !== ''){
+    talker.classList.add('kkoname');
+    talker.innerHTML = talk[0];
+  }
+  
+  if(talkTime !== ''){
+    talkTime.classList.add('kkodate');
+    talkTime.innerHTML = talk[1];
+  }
+  
+  talkContent.classList.add('kkotxt');
+  talkContent.innerHTML = talk[2];
+}
+
+// 버블 맞추기
+const prevtalk = [];
+function settingBubble(putTalk){
+  const $kkoul = document.createElement('ul');
+  $kkoul.classList.add('kTalkBox', 'active');
+  const $talker = document.createElement('li');
+  const $talkTime = document.createElement('li');
+  const $talkContent = document.createElement('li');
+  $txtLog.prepend($kkoul);
+  const $kTalkBoxes = $txtLog.querySelectorAll('.kTalkBox');
+  talkBubble($talker, $talkTime, $talkContent, putTalk);
+
+  function delkko(num){
+    if(prevtalk[0] === putTalk[0] && $kTalkBoxes.length>1) {
+      $kTalkBoxes[1].firstElementChild.remove();
+      $kTalkBoxes[1].classList.remove('active');
+      if(prevtalk[1] === putTalk[1]){
+        $kkoul.querySelectorAll('li')[num].remove();
+      }
+    }
+  };
+
+  if(putTalk[0] === talker[0]){ // false 인 경우에도 진행이 되는 이유???
+    $kkoul.classList.add('right');
+    $kkoul.append($talker, $talkTime, $talkContent);
+    delkko(1);
+  } else if(putTalk.length > 1 && talker.includes(putTalk[0])) {
+    $kkoul.classList.add('left');
+    $kkoul.append($talker, $talkContent, $talkTime);
+    delkko(2);
+  } else if(putTalk[0].includes("---------------")){
+    dateBubble(putTalk[0]);
+    $kkoul.append($wholeDate);
+  } else {
+    $kkoul.remove();
+  }
+  
+  prevtalk[0] = putTalk[0];
+  prevtalk[1] = putTalk[1];
+}
+
+//페이지 만들기
+let count = 0;
+let loadingCount = 1;
+function makepage(start,end) {
+  if(count > end) {
+    return;
+  }
+  settingBubble(bubbleSplit[count]);
+  count = count+1;
+  makepage(start,end);
+}
+
+
+// 파일 업로드 시, 처음 뜨는 내용
 $getFileBtn.addEventListener('change', showTxtFile)
 
 function showTxtFile(){
-  $secpg.innerHTML = '';
+  $txtLog.innerHTML = '';
+  bubbleSplit.length = 0;
+
   // 배열 타입이므로 0번째 파일 참조
   const file = $getFileBtn.files[0];
   const reader = new FileReader();
 
-  reader.onload = function(){
-    // log container 생성
-    $secpg.classList.add('show');
-    const addtxtLog = document.createElement("div");
-    addtxtLog.id = "txtlog";
-    $secpg.appendChild(addtxtLog);
-    const $txtLog = document.getElementById("txtlog");
+  // 텍스트 파일 형식으로 읽어오기
+  reader.readAsText(file, 'UTF-8');
 
-    // 불러온 내용 저장
-    const allTxt = reader.result;
-    
+  reader.onload = function(){
+    $secpg.classList.add('show'); // log container 보이기
+    fileChanges[0] = reader.result; // 새로 부른 파일 내용 저장
+
     // 대화 분리
-    const kakaoBubbles = allTxt.split("\r\n");
-    const bubbleSplit = [];
-    for(let v of kakaoBubbles){
+    fileChanges[1] = fileChanges[0].split("\r\n");
+    for(let v of fileChanges[1]){
       bubbleSplit.unshift(v.replaceAll("[","").split("] "));
     }
 
     // 대화 내 화자 정리
-    const talker = [];
     for(let a of bubbleSplit){
       if(a.length > 1 && !talker.includes(a[0])) {
         talker.push(a[0]);
       }
     }
-
-    // 날짜 구성
-    const $wholeDate = document.createElement('div');
-
-    function dateBubble(date){
-      let kkotxtdate = date.replaceAll("---------------","");
-      $wholeDate.classList.add('kkodatebox');
-      $wholeDate.innerHTML = kkotxtdate;
-    }
-
-    // 버블 구성
-    function talkBubble(talker, talkTime, talkContent, talk){
-      talker.classList.add('kkoname');
-      talker.innerHTML = talk[0];
-      
-      talkTime.classList.add('kkodate');
-      talkTime.innerHTML = talk[1];
-      
-      talkContent.classList.add('kkotxt');
-      talkContent.innerHTML = talk[2];
-    }
-
-    // 버블 맞추기
-    function settingBubble(putTalk){
-      const $kkoul = document.createElement('ul');
-      $kkoul.classList.add('kTalkBox');
-      const $talker = document.createElement('li');
-      const $talkTime = document.createElement('li');
-      const $talkContent = document.createElement('li');
-      
-      talkBubble($talker, $talkTime, $talkContent, putTalk);
-
-      console.log(putTalk[0] === talker[0]);
-      if(putTalk[0] === talker[0]){ // false 인 경우에도 진행이 되는 이유???
-        $kkoul.classList.add('right');
-        $kkoul.append($talker, $talkContent, $talkTime);
-      } else if(putTalk.length > 1 && talker.includes(putTalk[0])) {
-        $kkoul.classList.add('left');
-        $kkoul.append($talker, $talkContent, $talkTime);
-      } else if(putTalk[0].includes("---------------")){
-        dateBubble(putTalk[0]);
-        $kkoul.append($wholeDate);
-      }
-      
-      $txtLog.prepend($kkoul);
-    }
-    settingBubble(bubbleSplit[0]);
-    console.log($txtLog);
-    settingBubble(bubbleSplit[1]);
-    console.log($txtLog);
-    settingBubble(bubbleSplit[2]);
-    console.log($txtLog);
-
-
-    // 페이지 만들기
-    let count = 0;
-
-    makepage();
-    function makepage() {
-      if(count > 10) {
-        return;
-      }
-      settingBubble(bubbleSplit[count]);
-      console.log($kkoul, bubbleSplit[count]);
-      count = count+1;
-      makepage();
-    }
-
-
-    // 스크롤에 따라서 새로 로딩
-    
+    count = 0;
+    loadingCount = 1;
+    makepage(0,loadingAmount);
   }
-
-  // 텍스트 파일 형식으로 읽어오기
-  reader.readAsText(file, 'UTF-8');
 }
+
+// 스크롤에 따라서 새로 로딩
+// $txtLog.addEventListener('scroll', function(){
+//   console.log('123');
+// })
+
+// 스크롤에 따라 새로 로딩할 때, 새로 로딩할 때마다 추가 함수
+let checkCount;
+
+$txtLog.addEventListener('click', function(){
+  checkCount = count;
+  loadingCount = loadingCount+1;
+  makepage(checkCount,loadingAmount*loadingCount);
+})
