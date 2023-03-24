3721a8447c314a3d2d4385f64204a9453209b248
kingji8 <jiwon.c.king@gmail.com>
2023-03-24 18:17:06 +0900

    change(scroll)

--- main.js
+++ main.js
@@ -1,144 +1,112 @@
-const $getFileBtn = document.getElementById("getFileBtn");
-const $secpg = document.querySelector("#secPage .container");
+// 현재 채팅 인덱스와 텍스트 라인 배열을 추적하기 위한 변수 초기화
+let chatIndex = 0;
+let textLineArray = [];
+const prevdata = [],
+      talker = [];
+const txtLog = document.getElementById('txtlog');
+const detector = document.getElementById('detector');
 
-$getFileBtn.addEventListener('change', showTxtFile)
+// DOMContentLoaded 이벤트가 발생하면 실행되는 함수
+document.addEventListener('DOMContentLoaded', function() {
+    const textInput = document.getElementById('getFileBtn');
 
-function showTxtFile(){
-  $secpg.innerHTML = '';
-  // 배열 타입이므로 0번째 파일 참조
-  const file = $getFileBtn.files[0];
-  const reader = new FileReader();
+    // 파일 입력이 변경되면 실행되는 이벤트 리스너
+    textInput.addEventListener('change', function(e) {
+        const fileReader = new FileReader();
+        const file = e.target.files[0];
+        fileReader.readAsText(file,'UTF-8');
+        
+        // 파일 읽기가 완료되면 실행되는 이벤트 핸들러
+        fileReader.onload = function() {
+            const text = fileReader.result;
+            textArray = text.split('\r\n').reverse();
+            txtLog.classList.add("show")        
+        };
+    });
 
-  reader.onload = function(){
-    const addtxtLog = document.createElement("div");
-    addtxtLog.id = "txtlog";
-    $secpg.appendChild(addtxtLog);
-    const $txtLog = document.getElementById("txtlog");
+    // 교차점 관찰자 인스턴스 생성
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                loadChatBubbles(10); 
+            }
+        });
+    });
 
-    const talker = [];
-    let html, datehtml;
-    // let kkobubble = [],
-    //     kkotxtdate = [],
-    //     beforeData = [];
-    let count = 0;
-    let allTxt = reader.result;
+    // 교차점 관찰자가 detector 요소를 관찰하도록 설정
+    observer.observe(detector);
+});
 
-    // 화자 닉네임 구분하기
-    let startIndex = allTxt.indexOf("[")+1;
-    let endIndex   = allTxt.indexOf("]");
-    talker[0] = allTxt.substring(startIndex, endIndex);
-    let a = allTxt;
-    checkSecTalker();
-    function checkSecTalker(){
-      a = a.slice(endIndex+12);
-      startIndex = a.indexOf("[")+1;
-      endIndex = a.indexOf("]");
-      let b = a.substring(startIndex, endIndex);
+// 채팅 버블을 로드하는 함수
+function loadChatBubbles(count) {
+    const prevLastChatBubble = txtLog.firstElementChild.nextElementSibling;
+    const prevLastChatBubblePosition = prevLastChatBubble ? prevLastChatBubble.offsetTop : 0;
 
-      if(talker[0] == b){
-        checkSecTalker();
-      } else {
-        talker[1] = b;
-        return;
-      }
+    for (let i = 0; i < count; i++) {
+        if (chatIndex >= textArray.length) {
+            break;
+        }
+        const chatBubbleElement = chatBubble(textArray[chatIndex]);
+        detector.after(chatBubbleElement);
+        chatIndex++;
     }
 
-
-    makeTalk();
-
-    function makeTalk(){
-      // 대화상자 만들기
-      const $kkoul = document.createElement('ul');
-      const $kkodate = document.createElement('div');
-      $kkoul.classList.add('kTalkBox');
-      
-      // 대화 분리 (수정본)
-      const kakaoBubbles = allTxt.split("\r\n");
-      console.log(kakaoBubbles);
-
-
-
-      // 대화 분리
-      /*let checkfst = allTxt.indexOf("[")+1;
-      let checkscd = allTxt.indexOf("[", checkfst+11)+1;
-      if(checkscd > 0){
-        kkobubble = allTxt.substring(checkfst,checkscd-1);
-      } else {
-        kkobubble = allTxt;
-      }
-      kkobubble = kkobubble.replaceAll("[","");
-      kkobubble = kkobubble.split("]");
-      console.log(kkobubble);
-      
-
-      const $uls = document.querySelectorAll('#txtlog .kTalkBox');
-      let lastul = $uls[$uls.length-1];
-
-      // 화자 구별 후, li 만들기
-      if(kkobubble[0] == fstTalker){
-        $kkoul.classList.add('fst');
-        if ($uls.length >= 1 && beforeData[0] == kkobubble[0] && beforeData[1] == kkobubble[1]) {
-          lastul.lastChild.querySelector('.kkodate').remove();
-          html = "<li class='kkotxtbox fst'>";
-        } else {
-          $kkoul.classList.add('active');
-          html = "<li class='kkoname fst'>"+kkobubble[0]+"</li>";
-          html += "<li class='kkotxtbox fst'>";
-        }
-        if(kkobubble[2].includes("---------------")) {
-          kkotxtdate = kkobubble[2].split("---------------");
-          html += "<span class='kkotxt fst'>"+kkotxtdate[0]+"</span>";
-          html += "<span class='kkodate fst'>"+kkobubble[1]+"</span>";
-          html += "</li>"
-          datehtml = kkotxtdate[1];
-        } else {
-          html += "<span class='kkotxt fst'>"+kkobubble[2]+"</span>";
-          html += "<span class='kkodate fst'>"+kkobubble[1]+"</span>";
-          html += "</li>"
-        }
-      } else if (kkobubble[0] == secTalker) {
-        $kkoul.classList.add('scd');
-        if ($uls.length >= 1 && beforeData[0] == kkobubble[0] && beforeData[1] == kkobubble[1]) {
-          lastul.lastChild.querySelector('.kkodate').remove();
-          html = "<li class='kkotxtbox scd'>";
-        } else {
-          $kkoul.classList.add('active');
-          html = "<li class='kkoname scd'>"+kkobubble[0]+"</li>";
-          html += "<li class='kkotxtbox scd'>";
-        }
-        if(kkobubble[2].includes("---------------")) {
-          kkotxtdate = kkobubble[2].split("---------------");
-          html += "<span class='kkodate scd'>"+ kkobubble[1]+"</span>";
-          html += "<span class='kkotxt scd'>"+ kkotxtdate[0]+"</span>";
-          html += "</li>"
-          datehtml = kkotxtdate[1];
-        } else {
-          html += "<span class='kkodate scd'>"+kkobubble[1]+"</span>";
-          html += "<span class='kkotxt scd'>"+kkobubble[2]+"</span>";
-          html += "</li>"
-        }
-      }*/
-
-      // 텍스트 파일 붙이기
-      $kkoul.innerHTML = html;
-      $txtLog.appendChild($kkoul);
-      if (datehtml != null) {
-        $kkodate.classList.add("kkodatebox");
-        $kkodate.innerHTML = datehtml;
-        $txtLog.appendChild($kkodate);
-        datehtml = null;
-      }
-      allTxt = allTxt.slice(checkscd-1);
-      beforeData = [kkobubble[0], kkobubble[1], kkobubble[2]];
-      count++;
-
-      if (allTxt.length <= 1 || checkscd <= 0 || count >= 1000){
-        return;
-      } else {
-        makeTalk();
-      }
+    if (prevLastChatBubble) {
+        const newPosition = prevLastChatBubble.offsetTop;
+        const positionDifference = newPosition - prevLastChatBubblePosition;
+        txtLog.scrollTop += positionDifference - 700;
+    } else {
+        txtLog.scrollTop = txtLog.scrollHeight;
     }
-  };
-  // 텍스트 파일 형식으로 읽어오기
-  reader.readAsText(file, 'UTF-8');
+}
+
+// 채팅 버블 요소를 생성하는 함수
+function chatBubble(line) {
+    const kTalkBox = document.createElement('div');
+    kTalkBox.classList.add("kTalkBox");
+    const kTalkBoxes = txtLog.querySelectorAll('.kTalkBox');
+
+    if (line.includes("---")) { // 날짜
+        kTalkBox.innerHTML = `<div class="kkodatebox">${line.replaceAll("---------------", "").trim()}</div>`;
+        prevdata.length = 0;
+    } else {
+        if(line.includes("]")){  // 대화
+          const parts = line.split("]");
+          const name = parts[0].replace("[", "").trim();
+          const time = parts[1].replace("[", "").trim();
+          const message = parts[2].trim();
+
+          // 작성자 추가
+          if(talker.includes(name) == false){
+            talker.push(name);
+          }
+          // 왼쪽 오른쪽
+          if(name === talker[0]){
+            kTalkBox.classList.add("right");
+          } else {
+            kTalkBox.classList.add("left");
+          }
+
+          // 대화 생성
+          kTalkBox.classList.add("active");
+          kTalkBox.innerHTML = `<div class="kkoname">${name}</div>
+                                <div class="kkotxtbox">
+                                  <div class="kkotxt">${message}</div>
+                                  <div class="kkodate">${time}</div>
+                                </div>`;
+          
+          // 이전 대화에서 지워야 할 것
+          if(prevdata.length>0 && kTalkBoxes.length>1 && prevdata[0] == name){
+            kTalkBoxes[0].querySelector('.kkoname').remove();
+            kTalkBoxes[0].classList.remove('active');
+            if(prevdata[1] == time) {
+              kTalkBox.querySelector('.kkodate').remove();
+            }
+          }
+          
+          prevdata[0] = name;
+          prevdata[1] = time;
+        }
+    }
+    return kTalkBox;
 }
